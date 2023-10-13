1b0352997912904faae637718a71464c93450001
Sahil Kodwani <2020.sahil.kodwani@ves.ac.in>
2023-10-13 21:52:38 +0530

    Changed the Result.jsx

--- components/Result.js
+++ components/Result.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
+
 const Result = () => {
   const location = useLocation();
   const age = location.state.age;
   const name = location.state.name;
   const isEligible = age > 18;
+
 
   return (
     <div>
